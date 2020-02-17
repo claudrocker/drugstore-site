7c0ddb95cbf7bccd9146d305da66d40f08386434
Claudio Alvarez <calvarezt@gmail.com>
2020-02-16 22:48:44 -0300

    Make post request to communes service

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const qs = require('qs');
 
 const HTTP_PORT = 8000;
 const REGION_ID = 7; // Metropolitan region
+const COMMUNES_URL = 'https://midastest.minsal.cl/farmacias/maps/index.php/utilidades/maps_obtener_comunas_por_regiones';
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,10 +17,44 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
+/**************/
 /* Api routes */
+/***+++++++++**/
+
+/* Communes */
 app.get("/api/communes", async(req, res, next) => {
-    res.json({
-        "region_id": REGION_ID,
-        "message": "hello!"
-    })
+    try {
+        const communes = await getCommunes();
+        res.json({
+            "communes": communes
+        })
+    } catch(error) {
+        console.log(error);
+        res.json({
+            "error": true,
+            "message": "service unavailable"
+        })
+    }
 });
+
+async function getCommunes(){
+    let params = qs.stringify({
+        reg_id: REGION_ID
+    });
+
+    try {
+        const res = await axios({
+            method: 'post',
+            url: COMMUNES_URL,
+            data: params,
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        });
+        console.log(res.data);
+        return [1,2,3];
+    } catch(error) {
+        console.log(error);
+        return error;
+    }
+}
